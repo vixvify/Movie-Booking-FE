f09a40e8c7845191206af5785ebc93cfba8b4699
😺 <asnaweeezor@gmail.com>
2026-04-09 13:51:27 +0700

    feat: refactor authentication pages and enhance UI components for better user experience

--- src/components/Comingsoon.tsx
+++ src/components/Comingsoon.tsx
@@ -1,5 +1,49 @@
 "use client";
 
+import useEmblaCarousel from "embla-carousel-react";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import Image from "next/image";
+import { ComingSoonData } from "../data/mock-data";
+
 export default function Coming() {
-  return <></>;
+  const [emblaRef, emblaApi] = useEmblaCarousel({
+    loop: true,
+    align: "start",
+  });
+  return (
+    <div className="flex flex-col justify-center items-start px-20 mt-5">
+      <h1 className="text-white text-3xl font-semibold">Now Showing</h1>
+      <div className="flex justify-center items-center mt-10 gap-5 w-full">
+        <ArrowBackIosNewIcon
+          onClick={() => emblaApi?.scrollPrev()}
+          className="text-white cursor-pointer"
+        />
+        <div className="overflow-hidden w-full" ref={emblaRef}>
+          <div className="flex gap-x-5">
+            {ComingSoonData.map((item, index) => (
+              <div key={index} className="flex-[0_0_auto] mr-5 last:mr-10">
+                <Image
+                  src={item.poster}
+                  width={300}
+                  height={450}
+                  alt="poster"
+                  className="rounded-xl"
+                  key={index}
+                />
+                <h1 className="text-white text-xl font-semibold mt-4">
+                  {item.title}
+                </h1>
+                <h2 className="text-gray-300">{item.release}</h2>
+              </div>
+            ))}
+          </div>
+        </div>
+        <ArrowForwardIosIcon
+          onClick={() => emblaApi?.scrollNext()}
+          className="text-white cursor-pointer"
+        />
+      </div>
+    </div>
+  );
 }
---
+++ src/data/mock-data.ts
@@ -0,0 +1,147 @@
+export const HeaderPoster = ["/avatarheader.png", "/spiderheader.png"];
+
+export const MovieData = [
+  {
+    id: 1,
+    title: "No Other Choice",
+    poster:
+      "https://res.cloudinary.com/dxvaplm2o/image/upload/v1765892589/movies/gwg8dyjrvxwdjchvhsjr.png",
+    release: "5 มกราคม",
+  },
+  {
+    id: 2,
+    title: "ข้างบ้าน",
+    poster:
+      "https://res.cloudinary.com/dxvaplm2o/image/upload/v1765892568/movies/o86qu2ucfjzur7799dke.png",
+    release: "12 กุมภาพันธ์",
+  },
+  {
+    id: 3,
+    title: "ท่าแร่",
+    poster:
+      "https://res.cloudinary.com/dxvaplm2o/image/upload/v1765892519/movies/rhtfwy6zxrosoguc1our.png",
+    release: "19 กุมภาพันธ์",
+  },
+  {
+    id: 4,
+    title: "Weapons",
+    poster:
+      "https://res.cloudinary.com/dxvaplm2o/image/upload/v1765892492/movies/lyixd2d1nnrbbh1nryba.png",
+    release: "26 มีนาคม",
+  },
+  {
+    id: 5,
+    title: "The Long Walk",
+    poster:
+      "https://res.cloudinary.com/dxvaplm2o/image/upload/v1765892473/movies/dohn8qng0m2qtx4mrqlb.png",
+    release: "2 เมษายน",
+  },
+  {
+    id: 6,
+    title: "เสือ",
+    poster:
+      "https://res.cloudinary.com/dxvaplm2o/image/upload/v1765892451/movies/jxatbd8ijopnylwasuxi.png",
+    release: "9 เมษายน",
+  },
+  {
+    id: 7,
+    title: "ธี่หยด 3",
+    poster:
+      "https://res.cloudinary.com/dxvaplm2o/image/upload/v1765892416/movies/fzlote3efezboizkvrbh.png",
+    release: "1 เมษายน",
+  },
+  {
+    id: 8,
+    title: "Exit 8",
+    poster:
+      "https://res.cloudinary.com/dxvaplm2o/image/upload/v1765892398/movies/olehhspyremd6gm9n5ln.png",
+    release: "15 มกราคม",
+  },
+  {
+    id: 9,
+    title: "หลานม่า",
+    poster:
+      "https://res.cloudinary.com/dxvaplm2o/image/upload/v1765892338/movies/ikxri7w491vrw6ocfwqx.jpg",
+    release: "22 กุมภาพันธ์",
+  },
+  {
+    id: 10,
+    title: "ฮาลาบาลา",
+    poster:
+      "https://res.cloudinary.com/dxvaplm2o/image/upload/v1765892311/movies/fflcr4oxgg1lmob3kl9a.jpg",
+    release: "29 กุมภาพันธ์",
+  },
+];
+
+export const ComingSoonData = [
+  {
+    id: 11,
+    title: "Oppenheimer",
+    poster:
+      "https://res.cloudinary.com/dxvaplm2o/image/upload/v1765892291/movies/l2cikrt2jzlakywfdoza.jpg",
+    release: "20 เมษายน",
+  },
+  {
+    id: 12,
+    title: "A Mincraft Movie",
+    poster:
+      "https://res.cloudinary.com/dxvaplm2o/image/upload/v1765892233/movies/y6zbvz7difq280nerkoe.jpg",
+    release: "4 พฤษภาคม",
+  },
+  {
+    id: 13,
+    title: "อ้าย คนหล่อลวง",
+    poster:
+      "https://res.cloudinary.com/dxvaplm2o/image/upload/v1765892115/movies/xqzgnm4mpy4p8tkgbsxo.jpg",
+    release: "8 พฤศจิกายน",
+  },
+  {
+    id: 14,
+    title: "Thunderbolts",
+    poster:
+      "https://res.cloudinary.com/dxvaplm2o/image/upload/v1765892065/movies/zq0d1iywpbbjs53irq5n.jpg",
+    release: "15 มิถุนายน",
+  },
+  {
+    id: 15,
+    title: "Alien Romulus",
+    poster:
+      "https://res.cloudinary.com/dxvaplm2o/image/upload/v1765892045/movies/kykzqkpgnkrftxyntrr9.jpg",
+    release: "22 มิถุนายน",
+  },
+  {
+    id: 16,
+    title: "The Substance",
+    poster:
+      "https://res.cloudinary.com/dxvaplm2o/image/upload/v1765891948/movies/u3wyb6vmntlc2elreety.jpg",
+    release: "29 มิถุนายน",
+  },
+  {
+    id: 17,
+    title: "ธี่หยด 2",
+    poster:
+      "https://res.cloudinary.com/dxvaplm2o/image/upload/v1765891915/movies/s0maikwbsvfg5wcev4qm.jpg",
+    release: "6 กรกฎาคม",
+  },
+  {
+    id: 18,
+    title: "ธี่หยด",
+    poster:
+      "https://res.cloudinary.com/dxvaplm2o/image/upload/v1765891894/movies/koumvnukemvnnujaraxk.jpg",
+    release: "13 กรกฎาคม",
+  },
+  {
+    id: 19,
+    title: "วิมานหนาม",
+    poster:
+      "https://res.cloudinary.com/dxvaplm2o/image/upload/v1765891868/movies/lqalwgo6wtiporjjot6k.jpg",
+    release: "20 กรกฎาคม",
+  },
+  {
+    id: 20,
+    title: "วัยหนุ่ม 2544",
+    poster:
+      "https://res.cloudinary.com/dxvaplm2o/image/upload/v1765891845/movies/vacvx8tysgizvrxtyhjr.jpg",
+    release: "27 กรกฎาคม",
+  },
+];
